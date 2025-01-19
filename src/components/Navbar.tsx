70d34427228b55e080683644d2a8311f97a056c0
Leandro Licata <leandrolicata1@gmail.com>
2025-01-19 17:34:01 -0300

    navbar changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
@@ -6,26 +5,26 @@ export default function Navbar() {
     <nav className="bg-deep-blue text-aqua-mist p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center space-x-2">
-          <img src="/images/lean-market-mini.png" alt="Logo" className="h-10" />
+          <img src="/images/lean-market-mini.png" alt="Logo" className="h-10 mr-4" />
         </div>
 
         <div className="flex-1 mx-4">
           <input
             type="search"
             placeholder="Buscar..."
-            className="w-full px-4 py-2 rounded bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-[600px] px-4 py-2 rounded bg-white text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
 
         <div className="space-x-4">
-          <Link href="/home" className="hover:text-blue-400">
-            Inicio
+          <Link href="/products" className="hover:underline">
+            Productos
           </Link>
-          <Link href="/about" className="hover:text-blue-400">
-            Nosotros
+          <Link href="/cart" className="hover:underline">
+            Carrito
           </Link>
-          <Link href="/contact" className="hover:text-blue-400">
-            Contacto
+          <Link href="/about" className="hover:underline">
+            Acerca de
           </Link>
         </div>
       </div>
