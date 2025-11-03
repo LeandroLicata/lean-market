af4a1fcffa618ced52b02346dd8d26221f803cd8
Leandro Licata <leandrolicata1@gmail.com>
2025-11-03 08:21:42 -0300

    add getOrders on orderSlice

--- src/features/order/orderSlice.ts
+++ src/features/order/orderSlice.ts
@@ -1,17 +1,25 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { Order } from "@/types/order";
+import { Status } from "@/types/status";
+import { ErrorState } from "@/types/error";
 
 interface OrderState {
   orders: Order[];
-  status: "idle" | "loading" | "succeeded" | "failed";
-  error: string | null;
+  status: Record<"confirm" | "fetch", Status>;
+  error: Record<"confirm" | "fetch", ErrorState>;
 }
 
 const initialState: OrderState = {
   orders: [],
-  status: "idle",
-  error: null,
+  status: {
+    confirm: "idle",
+    fetch: "idle",
+  },
+  error: {
+    confirm: null,
+    fetch: null,
+  },
 };
 
 export const confirmOrder = createAsyncThunk<Order>(
@@ -22,22 +30,44 @@ export const confirmOrder = createAsyncThunk<Order>(
   }
 );
 
+export const getOrders = createAsyncThunk<Order[]>(
+  "orders/fetchOrders",
+  async () => {
+    const response = await axios.get("/api/orders");
+    return response.data;
+  }
+);
+
 const orderSlice = createSlice({
   name: "orders",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(confirmOrder.pending, (state) => {
-      state.status = "loading";
-      state.error = null;
+      state.status.confirm = "loading";
+      state.error.confirm = null;
     });
     builder.addCase(confirmOrder.fulfilled, (state) => {
-      state.status = "succeeded";
-      state.error = null;
+      state.status.confirm = "succeeded";
+      state.error.confirm = null;
     });
     builder.addCase(confirmOrder.rejected, (state, action) => {
-      state.status = "failed";
-      state.error = action.error.message || "Failed to confirm the order";
+      state.status.confirm = "failed";
+      state.error.confirm =
+        action.error.message || "Failed to confirm the order";
+    });
+    builder.addCase(getOrders.pending, (state) => {
+      state.status.fetch = "loading";
+      state.error.fetch = null;
+    });
+    builder.addCase(getOrders.fulfilled, (state, action) => {
+      state.status.fetch = "succeeded";
+      state.orders = action.payload;
+      state.error.fetch = null;
+    });
+    builder.addCase(getOrders.rejected, (state, action) => {
+      state.status.fetch = "failed";
+      state.error.fetch = action.error.message || "Failed to fetch orders";
     });
   },
 });
